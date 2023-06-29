da56405d08b160bb3cc890f1f5eea48856ba4619
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-06-29 02:51:49 +0200

    chore: add rustup pkg

--- home.nix
+++ home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let 
-  username = "delphai";
+  username = "piankris";
   homeDirectory = "/home/${username}";
   nullPackage = name: pkgs.writeShellScriptBin name "";
 in
@@ -9,107 +9,30 @@ in
   home = {
     inherit username homeDirectory;
     stateVersion = "22.05";
-    sessionVariables = {
-      TERMINAL = "kitty";
-      BROWSER = "brave";
-      EDITOR = "code";
-    };
-    sessionPath = [
-      "${homeDirectory}/.pyenv/bin"
+    packages = with pkgs; [
+      lsd
+      bat
+      nerdfonts
+      joypixels
+      awscli
+      rustup
     ];
-    packages = [
-      pkgs.lsd
-      pkgs.bat
-      pkgs.nerdfonts
+    sessionPath = [
+      "${homeDirectory}/Work/data-delivery-backend/.venv/bin/sqlfluff"
+      "${homeDirectory}/slides"
+      "${homeDirectory}/act"
     ];
     shellAliases = {
       ls="lsd -la";
       lsd="lsd -la";
       rm="rm -f";
       home="code ~/.config/nixpkgs/home.nix";
+      mnt="cd /mnt/c/Users/pianykri/'OneDrive - diconium GmbH'";
+      desktop="cd /mnt/c/Users/pianykri/'OneDrive - diconium GmbH'/Desktop";
+      ipython="ipython3";
     };
   };
   fonts.fontconfig.enable = true;
-
-  programs.kitty = {
-    enable = true;
-    package = nullPackage "kittyNull";
-    theme = "Tokyo Night Storm";
-    font = {
-      name = "Fira Code Nerd Font";
-      size = 12;
-    };
-    extraConfig = ''
-      allow_remote_control yes
-      background_opacity 0.93
-      # background_image ${homeDirectory}/nord_valley1.png
-      # background_image_opacity 0.9
-      # background_image_layout scaled
-      linux_display_server x11
-
-      # foreground            #D8DEE9
-      # background            #2E3440
-      # selection_foreground  #000000
-      # selection_background  #FFFACD
-      # url_color             #0087BD
-      # cursor                #81A1C1
-
-      # # black
-      # color0   #3B4252
-      # color8   #4C566A
-
-      # # red
-      # color1   #BF616A
-      # color9   #BF616A
-
-      # # green
-      # color2   #A3BE8C
-      # color10  #A3BE8C
-
-      # # yellow
-      # color3   #EBCB8B
-      # color11  #EBCB8B
-
-      # # blue
-      # color4  #81A1C1
-      # color12 #81A1C1
-
-      # # magenta
-      # color5   #B48EAD
-      # color13  #B48EAD
-
-      # # cyan
-      # color6   #88C0D0
-      # color14  #8FBCBB
-
-      # # white
-      # color7   #E5E9F0
-      # color15  #ECEFF4
-
-      map kitty_mod+p next_window # kitty_mod : shift + cntrl
-    '';
-    settings = {
-      window_padding_width = "8 8";
-    };
-  };
-
-#   programs.git = {
-#     enable = true;
-#     userName = "Kristina Pianykh";
-#     userEmail = "kristinavrnrus@gmail.com";
-#     diff-so-fancy.enable = true;
-#     includes = [
-#       { 
-#         condition = "gitdir:${directories.delphai.path}/";
-#         contents = {
-#           user = {
-#             name = "Kristina Pianykh";
-#             email = "kristina@delphai.com";
-#           };
-#         };
-#       }
-#     ];
-#   };
 
   programs.starship = {
     enable = true;
@@ -122,9 +45,13 @@ in
     history.extended = true;
     dotDir = ".config/zsh";
     initExtraFirst = ''
-      export PYENV_ROOT="$HOME/.pyenv"
-      command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-      eval "$(pyenv init -)"
+      export GPG_TTY=$(tty)
+      
+      eval "$(ssh-agent -s)"
+      ssh-add ~/.ssh/work
+
+      eval "$(direnv hook zsh)"
+      eval "$(direnv stdlib)"
     '';
     prezto = {
       enable = true;
@@ -144,4 +71,36 @@ in
     };
   };
   programs.home-manager.enable = true;
+  
+  programs.git = {
+  enable = true;
+  userName = "Kristina Pianykh";
+  userEmail = "kristina.pianykh@diconium.com";
+  signing = {
+    key = "3A09BEC8E7DCA833";
+    gpgPath = "/usr/bin/gpg";
+    signByDefault = true;
+  };
+  diff-so-fancy.enable = true;
+  extraConfig = {
+    init = {
+        defaultBranch = "main";
+    };
+  };
+  includes = [
+  { 
+    condition = "gitdir:${homeDirectory}/Private/";
+    contents = {
+      user = {
+        name = "Kristina Pianykh";
+        email = "kristinavrnrus@gmail.com";
+        signingKey = "C66C7DFC66E169F1";
+      };
+      commit = {
+        gpgSign = true;
+      };
+    };
+  }
+  ];
+  };
 }
