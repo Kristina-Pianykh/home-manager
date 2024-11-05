4f3dbb694c5b1c0c9a865ce8bdac653e21a5018b
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-11-05 17:19:17 +0100

    chore: add reference to source for pretty git log output

--- git_log_alias.sh
+++ git_log_alias.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# taken from https://github.com/mrnugget/dotfiles/blob/d2d17985890b0d74f04abfccd56620067643c9f6/githelpers
+
 HASH="%C(always,yellow)%h%C(always,reset)"
 RELATIVE_TIME="%C(always,green)%ar%C(always,reset)"
 AUTHOR="%C(always,bold blue)%an%C(always,reset)"
