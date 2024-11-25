de2c2ad8fa1ad0600430735d2b71e0144a0a49fe
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-11-25 03:34:07 +0100

    chore: remove macos zsh config options

--- zsh.nix
+++ zsh.nix
@@ -9,25 +9,10 @@
     dotDir = ".config/zsh";
 
     initExtraFirst = ''
+      export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+      export GNUPGHOME="~/.gnupg"
       export GPG_TTY=$(tty)
 
-      if [ -z "$SSH_AUTH_SOCK" ] && [ -z "$SSH_AGENT_PID" ]; then
-        # If no SSH Agent is running, start one and load keys from Apple keychain
-        eval "$(ssh-agent -s)"
-        ssh-add --apple-load-keychain
-      else
-        if [ -z "$(ssh-add -l | grep SHA256)" ]; then
-          # If agent is running but has no keys, load keys from Apple keychain
-          ssh-add --apple-load-keychain
-        fi
-      fi
-
-      bindkey "^[[1;5C" forward-word
-      bindkey "^[[1;5D" backward-word
-    '';
-
-    profileExtra = ''
-      eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
 
     prezto = {
