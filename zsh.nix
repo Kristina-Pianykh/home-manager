81419d3e4549c814fda1a3be9903158fece4250f
Kristina Pianykh <kristinavrnrus@gmail.com>
2025-04-26 23:39:43 +0200

    fix: copying in zsh vi mode to system clipboard

--- zsh.nix
+++ zsh.nix
@@ -12,20 +12,22 @@
     history.extended = true;
     dotDir = ".config/zsh";
 
-    initContent = lib.mkBefore ''
-      export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
-      export GNUPGHOME="~/.gnupg"
-      export GPG_TTY=$(tty)
-
-      function y() {
-        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-        yazi "$@" --cwd-file="$tmp"
-        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-          builtin cd -- "$cwd"
-        fi
-        rm -f -- "$tmp"
-      }
-    '';
+    initContent = lib.mkMerge [
+      (lib.mkBefore ''
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+        export GNUPGHOME="~/.gnupg"
+        export GPG_TTY=$(tty)
+      '')
+      (lib.mkAfter ''
+        # hack for copying to system clipboard https://github.com/jeffreytse/zsh-vi-mode/issues/19#issuecomment-1145784731
+        # use another copy util for macos
+        zvm_vi_yank () {
+          zvm_yank
+          echo -en ''${CUTBUFFER} |  wl-copy -n
+          zvm_exit_visual_mode ''${1:-true}
+        }
+      '')
+    ];
 
     prezto = {
       enable = true;
